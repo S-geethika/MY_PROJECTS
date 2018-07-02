0129ae8cabca1183f42bfbb59e4a100b73f952b1
S-geethika <geethika1808@gmail.com>
2018-07-02 15:39:12 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,204 @@
+
+import requests
+from io import BytesIO
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import warnings
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+import math
+import time
+import re
+import os
+import seaborn as sns
+from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity  
+from sklearn.metrics import pairwise_distances
+from matplotlib import gridspec
+from scipy.sparse import hstack
+import plotly
+import plotly.figure_factory as ff
+from plotly.graph_objs import Scatter, Layout
+plotly.offline.init_notebook_mode(connected=True)
+warnings.filterwarnings("ignore")
+import pickle
+import json
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+from keras import applications
+from sklearn.metrics import pairwise_distances
+import requests
+from PIL import Image
+
+def display_img(url,ax,fig):
+    # we get the url of the apparel and download it
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    # we will display it in notebook 
+    plt.imshow(img)
+
+data = pd.read_pickle('16k_apperal_data_preprocessed')
+with open('word2vec_model', 'rb') as handle:
+    model = pickle.load(handle)
+idf_title_vectorizer = CountVectorizer()
+idf_title_features = idf_title_vectorizer.fit_transform(data['title'])
+
+
+vocab = model.keys()
+def get_word_vec(sentence, doc_id, m_name):
+    vec = []
+    for i in sentence.split():
+        if i in vocab:
+            if m_name == 'weighted' and i in  idf_title_vectorizer.vocabulary_:
+                vec.append(idf_title_features[doc_id, idf_title_vectorizer.vocabulary_[i]] * model[i])
+            elif m_name == 'avg':
+                vec.append(model[i])
+        else:
+            vec.append(np.zeros(shape=(300,)))
+    return  np.array(vec)
+
+def get_distance(vec1, vec2):
+    final_dist = []
+    # for each vector in vec1 we caluclate the distance(euclidean) to all vectors in vec2
+    for i in vec1:
+        dist = []
+        for j in vec2:
+            # np.linalg.norm(i-j) will result the euclidean distance between vectors i, j
+            dist.append(np.linalg.norm(i-j))
+        final_dist.append(np.array(dist))
+    return np.array(final_dist)
+  
+def build_avg_vec(sentence, num_features, doc_id, m_name):
+    featureVec = np.zeros((num_features,), dtype="float32")
+    nwords = 0
+    
+    for word in sentence.split():
+        nwords += 1
+        if word in vocab:
+            if m_name == 'weighted' and word in  idf_title_vectorizer.vocabulary_:
+                featureVec = np.add(featureVec, idf_title_features[doc_id, idf_title_vectorizer.vocabulary_[word]] * model[word])
+            elif m_name == 'avg':
+                featureVec = np.add(featureVec, model[word])
+    if(nwords>0):
+        featureVec = np.divide(featureVec, nwords)
+    # returns the avg vector of given sentance, its of shape (1, 300)
+    return featureVec
+
+
+doc_id = 0
+w2v_title = []
+# for every title we build a avg vector representation
+for i in data['title']:
+    w2v_title.append(build_avg_vec(i, 300, doc_id,'avg'))
+    doc_id += 1
+
+w2v_title = np.array(w2v_title)
+# in the give heat map, each cell contains the euclidean distance between words i, j
+# we use the list of stop words that are downloaded from nltk lib.
+stop_words = set(stopwords.words('english'))
+#print ('list of stop words:', stop_words)
+
+data['brand'].fillna(value="Not given", inplace=True )
+
+# replace spaces with hypen
+brands = [x.replace(" ", "-") for x in data['brand'].values]
+types = [x.replace(" ", "-") for x in data['product_type_name'].values]
+colors = [x.replace(" ", "-") for x in data['color'].values]
+
+brand_vectorizer = CountVectorizer()
+brand_features = brand_vectorizer.fit_transform(brands)
+
+type_vectorizer = CountVectorizer()
+type_features = type_vectorizer.fit_transform(types)
+
+color_vectorizer = CountVectorizer()
+color_features = color_vectorizer.fit_transform(colors)
+extra_features = hstack((brand_features, type_features, color_features)).tocsr()
+
+
+
+def heat_map_w2v_brand(sentance1, sentance2, url, doc_id1, doc_id2, df_id1, df_id2, model):
+    s1_vec = get_word_vec(sentance1, doc_id1, model)
+    s2_vec = get_word_vec(sentance2, doc_id2, model)
+    s1_s2_dist = get_distance(s1_vec, s2_vec)
+   
+    data_matrix = [['Asin','Brand', 'Color', 'Product type'],
+               [data['asin'].loc[df_id1],brands[doc_id1], colors[doc_id1], types[doc_id1]], # input apparel's features
+               [data['asin'].loc[df_id2],brands[doc_id2], colors[doc_id2], types[doc_id2]]] # recommonded apparel's features
+    
+    colorscale = [[0, '#1d004d'],[.5, '#f2e5ff'],[1, '#f2e5d1']] 
+    
+    # we create a table with the data_matrix
+    table = ff.create_table(data_matrix, index=True, colorscale=colorscale)
+    # plot it with plotly
+    plotly.offline.iplot(table, filename='simple_table')
+    
+    # devide whole figure space into 25 * 1:10 grids
+    gs = gridspec.GridSpec(25, 15)
+    fig = plt.figure(figsize=(25,5))
+    ax1 = plt.subplot(gs[:, :-5])
+    # ploting the heap map based on the pairwise distances
+    ax1 = sns.heatmap(np.round(s1_s2_dist,6), annot=True)
+    # set the x axis labels as recommended apparels title
+    ax1.set_xticklabels(sentance2.split())
+    # set the y axis labels as input apparels title
+    ax1.set_yticklabels(sentance1.split())
+    # set title as recommended apparels title
+    ax1.set_title(sentance2)
+
+    # in last 25 * 10:15 grids we display image
+    ax2 = plt.subplot(gs[:, 10:16])
+    # we dont display grid lins and axis labels to images
+    ax2.grid(False)
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    
+    # pass the url it display it
+    display_img(url, ax2, fig)
+    
+    plt.show()
+
+
+doc_id = 0
+w2v_title_weight = []
+# for every title we build a weighted vector representation
+for i in data['title']:
+    w2v_title_weight.append(build_avg_vec(i, 300, doc_id,'weighted'))
+    doc_id += 1
+# w2v_title = np.array(# number of doc in courpus * 300), each row corresponds to a doc 
+w2v_title_weight = np.array(w2v_title_weight)
+
+
+bottleneck_features_train = np.load('16k_data_cnn_features.npy')
+asins = np.load('16k_data_cnn_feature_asins.npy')
+asins = list(asins)
+
+df_asins = list(data['asin'])
+
+
+def engine(doc_id, w_t, w_c, w_b,w_i, num_results):
+    title_dist  = pairwise_distances(w2v_title_weight, w2v_title_weight[doc_id].reshape(1,-1))
+    color_dist = pairwise_distances(color_features, color_features[doc_id])
+    brand_dist = pairwise_distances(brand_features, brand_features[doc_id])
+    doc_id = asins.index(df_asins[doc_id])
+    image_dist = pairwise_distances(bottleneck_features_train, bottleneck_features_train[doc_id].reshape(1,-1))
+    pairwise_dist   = (w_t * title_dist +  w_c * color_dist + w_b*brand_dist+w_i * image_dist)/float(w_t + w_c+w_b + w_i)
+    indices = np.argsort(pairwise_dist.flatten())[0:num_results]
+    pdists  = np.sort(pairwise_dist.flatten())[0:num_results]
+    df_indices = list(data.index[indices])
+    
+
+    for i in range(0, len(indices)):
+        heat_map_w2v_brand(data['title'].loc[df_indices[0]],data['title'].loc[df_indices[i]], data['medium_image_url'].loc[df_indices[i]], indices[0], indices[i],df_indices[0], df_indices[i], 'weighted')
+        print('ASIN :',data['asin'].loc[df_indices[i]])
+        print('Brand :',data['brand'].loc[df_indices[i]])
+        print('euclidean distance from input :', pdists[i])
+        print('='*125)
+
+engine(12566, 50, 20, 5, 10, 20)
